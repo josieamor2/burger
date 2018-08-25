69dc5acd5392e1a5bc70453bcd1849a57ffb1db7
Josephine Aguilar <josieamor@Josephines-MacBook-Pro.local>
2018-08-25 00:43:55 -0600

    burger start

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Kangaroo Burger');
+
+SELECT * FROM burgers;
